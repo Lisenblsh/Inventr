77b60d0975278c8fb39921add4d2c92b41292d00
Rin Kaoki <lemi013794@gmail.com>
2021-04-08 17:35:21 +0700

    08.04.2021

---
+++ Inventr/Authorization.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Inventr
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        Func func = new Func();
+        public int PRole;
+        protected override void OnClosed(EventArgs e) //Функция для завершеия отладки программы
+        {
+            base.OnClosed(e);
+
+            Application.Current.Shutdown();
+        }
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PRole = func.Authorez(TBox_Login.Text, PBox_Pass.Password);
+            TBox_Login.Clear();
+            PBox_Pass.Clear();
+            if (PRole != 0)
+                this.Hide();
+        }
+
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Button_Click(sender, e);
+            }
+        }
+    }
+    public class Func
+    {
+        public int Authorez(string Login, string Pass)
+        {
+            int role = 1;
+
+            //if (Login == "log" && Pass == "pass")
+            //{
+            //    role = 1;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Неверный логин или пароль");
+            //}
+            return role;
+        }
+    }
+}
--- Inventr/MainWindow.xaml.cs
+++ Inventr/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Inventr
     /// </summary>
     public partial class MainWindow : Window
     {
+        Authorization authorization = new Authorization();
         public MainWindow()
         {
             InitializeComponent();
+            this.Show();
+            authorization.Owner = this;
+            authorization.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Show();
+            authorization.Owner = this;
+            authorization.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Page1Kab1.Height = 418;
+            Page1Kab1.Width = 792;
+            Page1Kab1.Navigate(new Page1Kab1());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Page2Kab2.Height = 418;
+            Page2Kab2.Width = 792;
+            Page2Kab2.Navigate(new Page2Kab2());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Page3Kab3.Height = 418;
+            Page3Kab3.Width = 792;
+            Page3Kab3.Navigate(new Page3Kab3());
         }
     }
 }
